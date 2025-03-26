96e3be3b143d984c2f902e6b78fe9dca20382cb3
frc1987 <broncobots@gmail.com>
2025-03-25 19:22:50 -0500

    spotless

--- src/main/java/frc/robot/util/NetworkTableTimer.java
+++ src/main/java/frc/robot/util/NetworkTableTimer.java
@@ -5,16 +5,16 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 
 public class NetworkTableTimer {
-    public static Runnable wrap(String label, Runnable runnable) {
-        NetworkTableEntry entry = NetworkTableInstance.getDefault().getTable("timing").getEntry(label);
-        
-        return new Runnable() {
-            @Override
-            public void run() {
-                double startTime = Timer.getFPGATimestamp();
-                runnable.run();
-                entry.setDouble(Timer.getFPGATimestamp() - startTime);
-            }
-        };
-    }
+  public static Runnable wrap(String label, Runnable runnable) {
+    NetworkTableEntry entry = NetworkTableInstance.getDefault().getTable("timing").getEntry(label);
+
+    return new Runnable() {
+      @Override
+      public void run() {
+        double startTime = Timer.getFPGATimestamp();
+        runnable.run();
+        entry.setDouble(Timer.getFPGATimestamp() - startTime);
+      }
+    };
+  }
 }
