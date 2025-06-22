c1cd255beb94a26b1c1c569d9d97c866357bc74c
Lithiuman <lithiumxn@gmail.com>
2025-06-22 15:03:30 -0500

    Use MT1 dist < 0.65

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -38,36 +38,53 @@ public class Vision extends SubsystemBase {
     LimelightHelpers.SetRobotOrientation(
         LIMELIGHT_SCORING_NAME, yaw.getAsDouble(), 0.0, 0.0, 0.0, 0.0, 0.0);
 
-    LimelightHelpers.PoseEstimate scoringEst = updateAndAcquirePoseEstimate(LIMELIGHT_SCORING_NAME);
-    LimelightHelpers.PoseEstimate intakeEst = updateAndAcquirePoseEstimate(LIMELIGHT_INTAKE_NAME);
-    if (!processScoringLimelight(scoringEst)) {
-      processIntakeLimelight(intakeEst);
+    LimelightHelpers.PoseEstimate scoringEst = getMegaTag2(LIMELIGHT_SCORING_NAME);
+    LimelightHelpers.PoseEstimate intakeEst = getMegaTag2(LIMELIGHT_INTAKE_NAME);
+    if (!processScoring(scoringEst)) {
+      processIntake(intakeEst);
     }
   }
 
-  public boolean processScoringLimelight(LimelightHelpers.PoseEstimate estimate) {
-
-    if (estimate != null
-        && estimate.rawFiducials != null
-        && estimate.rawFiducials.length > 0
-        && estimate.avgTagDist < 3.75) {
-      double confidence = maPoseConfidence(estimate);
-      if (shouldUpdatePose()) {
-        RobotContainer.DRIVETRAIN.addVisionMeasurement(
-            estimate.pose,
-            estimate.timestampSeconds,
-            VecBuilder.fill(confidence, confidence, 99999999));
+  public boolean processScoring(LimelightHelpers.PoseEstimate estimate) {
+    if (estimate != null && estimate.rawFiducials != null && estimate.rawFiducials.length > 0) {
+      if (estimate.avgTagDist < 0.65) {
+        // MT1 Angle
+        estimate = getMegaTag1(LIMELIGHT_SCORING_NAME);
+        double confidence = maPoseConfidence(estimate);
+        if (shouldUpdatePose()) {
+          RobotContainer.DRIVETRAIN.addVisionMeasurement(
+              estimate.pose,
+              estimate.timestampSeconds,
+              VecBuilder.fill(confidence, confidence, 0.0001));
+        }
+        if (RobotContainer.DEBUG) {
+          DogLog.log("Vision/ScoringProcess/MT1Pose", estimate.pose);
+          DogLog.log("Vision/ScoringProcess/MT1Degrees", estimate.pose.getRotation().getDegrees());
+          DogLog.log("Vision/ScoringProcess/MT1Confidence", confidence);
+          DogLog.log("Vision/ScoringProcess/MT1Active", true);
+        }
+        return true;
+      } else if (estimate.avgTagDist < 3.75) {
+        // MT2 Regular
+        double confidence = maPoseConfidence(estimate);
+        if (shouldUpdatePose()) {
+          RobotContainer.DRIVETRAIN.addVisionMeasurement(
+              estimate.pose,
+              estimate.timestampSeconds,
+              VecBuilder.fill(confidence, confidence, 99999999));
+        }
+        if (RobotContainer.DEBUG) {
+          DogLog.log("Vision/ScoringProcess/MT2Pose", estimate.pose);
+          DogLog.log("Vision/ScoringProcess/MT2Confidence", confidence);
+          DogLog.log("Vision/ScoringProcess/MT1Active", false);
+        }
+        return true;
       }
-      if (RobotContainer.DEBUG) {
-        DogLog.log("Vision/ScoringProcess/Pose", estimate.pose);
-        DogLog.log("Vision/ScoringProcess/Confidence", confidence);
-      }
-      return true;
     }
     return false;
   }
 
-  public boolean processIntakeLimelight(LimelightHelpers.PoseEstimate estimate) {
+  public boolean processIntake(LimelightHelpers.PoseEstimate estimate) {
 
     if (estimate != null
         && estimate.rawFiducials != null
@@ -89,8 +106,12 @@ public class Vision extends SubsystemBase {
     return false;
   }
 
-  public LimelightHelpers.PoseEstimate updateAndAcquirePoseEstimate(String LIMELIGHT_NAME) {
+  public LimelightHelpers.PoseEstimate getMegaTag2(String LIMELIGHT_NAME) {
     return LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(LIMELIGHT_NAME);
+  }
+
+  public LimelightHelpers.PoseEstimate getMegaTag1(String LIMELIGHT_NAME) {
+    return LimelightHelpers.getBotPoseEstimate_wpiBlue(LIMELIGHT_NAME);
   }
 
   public void log() {}
